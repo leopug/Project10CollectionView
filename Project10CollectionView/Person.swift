c79dd030fb1f072ade82833f1ce5397a015ce8d9
leonardo maia pugliese <leo.pug@hotmail.com>
2020-01-19 18:21:21 -0300

    first commit

---
+++ Project10CollectionView/Person.swift
@@ -0,0 +1,21 @@
+//
+//  Person.swift
+//  Project10CollectionView
+//
+//  Created by Ana Caroline de Souza on 19/01/20.
+//  Copyright © 2020 Ana e Leo Corp. All rights reserved.
+//
+
+import UIKit
+
+class Person: NSObject {
+    
+    var name : String
+    var image : String
+    
+    init(name: String, image: String){
+        self.name = name
+        self.image = image
+    }
+    
+}
---
+++ Project10CollectionView/PersonCell.swift
@@ -0,0 +1,17 @@
+//
+//  PersonCell.swift
+//  Project10CollectionView
+//
+//  Created by Ana Caroline de Souza on 18/01/20.
+//  Copyright © 2020 Ana e Leo Corp. All rights reserved.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+    
+}
